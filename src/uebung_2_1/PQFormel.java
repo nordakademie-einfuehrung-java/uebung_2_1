d2ab0a51be650560bb6d01030a5e91f373b35896
14367 <14367@A001-11.nordakademie.intern>
2015-10-27 10:57:42 +0100

    PQ-Formel mit zusätzlicher Wahrheitsprüfung.

--- src/uebung_2_1/PQFormel.java
+++ src/uebung_2_1/PQFormel.java
@@ -4,17 +4,28 @@ public class PQFormel {
 
 	public static void main(String[] args) {
 	double p = 8;
-	double q = 15;
+	double q = 16;
 	
 	double unterDerWurzel = ((p/2)*(p/2))-q;
 	double vorDerWurzel	= -1*p/2;
 	
-	double loesung1 = vorDerWurzel+Math.sqrt(unterDerWurzel);
-	double loesung2 = vorDerWurzel-Math.sqrt(unterDerWurzel);
+	if ( unterDerWurzel < 0)
 	
-	System.out.println("x1 ="+loesung1);
-	System.out.println("x2 ="+loesung2);
-
+	{
+		System.out.println("Es gibt keine Lösung.");
+	}
+		else if ( unterDerWurzel == 0)
+		{
+			System.out.println("Die einzige Lösung beträgt " +vorDerWurzel);
+		}
+		else 
+		{
+			double loesung1 = vorDerWurzel+Math.sqrt(unterDerWurzel);
+			double loesung2 = vorDerWurzel-Math.sqrt(unterDerWurzel);
+			
+			System.out.println("x1 ="+loesung1);
+			System.out.println("x2 ="+loesung2);
+		}
 	}
 
 }
