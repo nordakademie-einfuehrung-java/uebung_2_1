9bafa1536843413d5a33f15c4c7a6f68fef35489
Ben6363 <15352@nordakademie.de>
2017-11-02 11:42:03 +0100

    Übung 1 programmiert. Keine negativer Wert unter der Wurzel wird mehr zugelassen.

--- src/uebung_2_1/QuadratWurzel.java
+++ src/uebung_2_1/QuadratWurzel.java
@@ -6,16 +6,23 @@ public class QuadratWurzel {
 		// TODO Auto-generated method stub
 		
 		double p = 5;
-		double q = 5;
+		double q = 100;
 		
 		double unterderwurzel = (((p/2)*(p/2))-q);
-		double vorderwurzel = (-p/2);
-		double x1 = (vorderwurzel - Math.sqrt(unterderwurzel));
-		double x2 = (vorderwurzel + Math.sqrt(unterderwurzel));
+		if (unterderwurzel < 0 ) {
+			System.out.println("Es ist keine Lösung möglich.");}
+			else {
+				double vorderwurzel = (-p/2);
+				double x1 = (vorderwurzel - Math.sqrt(unterderwurzel));
+				double x2 = (vorderwurzel + Math.sqrt(unterderwurzel));
+				System.out.println("Die Ergebnisse:");
+				System.out.println("x1 = " +x1);
+				System.out.println("x2 = " +x2);
+			};
 		
-		System.out.println("Die Ergebnisse:");
-		System.out.println("x1 = " +x1);
-		System.out.println("x2 = " +x2);
+		
+		
+		
 	}
 
 }
